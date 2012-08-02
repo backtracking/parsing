ba0ddbbf467cfc109c961820ffa197d853e8f8ac
U-Vincent-PC\Vincent <Vincent@Vincent-PC.(none)>
2012-08-02 18:07:43 +0200

    fix typo

--- VisualStudioIntegration/Java Integration/Java.EditorExtensions/Java.EditorExtensions/Parsing/Tokenizer.cs
+++ VisualStudioIntegration/Java Integration/Java.EditorExtensions/Java.EditorExtensions/Parsing/Tokenizer.cs
@@ -5,7 +5,7 @@
  * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
  * copy of the license can be found in the License.html file at the root of this distribution. If 
  * you cannot locate the Apache License, Version 2.0, please send an email to 
- * vspython@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+ * contact@smartmobili.com. By using this source code in any fashion, you are agreeing to be bound 
  * by the terms of the Apache License, Version 2.0.
  *
  * You must not remove this notice, or any other, from this software.
@@ -26,9 +26,8 @@ namespace Java.EditorExtensions
 {
 
     /// <summary>
-    /// IronJava tokenizer
+    /// Tokenizer
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Tokenizer")]
     public sealed partial class Tokenizer {
         private readonly JavaLanguageVersion _langVersion;
         private State _state;
